61158bee5cfcdc4b800cbd2e659a1bd51ed24e27
Persada Ersad <ersad40@gmail.com>
2019-10-21 19:58:31 +0700

    Master Product
     - Add CRUD table products
    
     - Add migration create table products
    
    Trello: https://trello.com/c/FRdl61KX
    
    update code

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_17_155622) do
+ActiveRecord::Schema.define(version: 2019_10_21_124934) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.string "code"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "roles", force: :cascade do |t|
     t.string "name"
