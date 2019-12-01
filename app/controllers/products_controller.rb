49dce0b4822ea2ba040187281661b89de02a0215
Persada Ersad <ersad40@gmail.com>
2019-12-01 17:15:07 +0700

    Add Category on Products
     - Add category_id to products
    
     - Add relation between category and product
    
    Trello: https://trello.com/c/aFtyBzdl

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -46,6 +46,6 @@ class ProductsController < ApplicationController
   end
 
   def product_params
-    params.require(:product).permit(:name, :description, :image)
+    params.require(:product).permit(:name, :description, :image, :category_id)
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
   has_many :packages
+  belongs_to :category
   mount_uploader :image, ImageUploader
   validates :name, :image, presence: true
 end
