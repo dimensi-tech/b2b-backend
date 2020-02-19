9e07ba027879c09a5b69852ae67058412cc289b4
Persada Ersad <ersad40@gmail.com>
2020-02-19 19:45:14 +0700

    Add Status to Booking
     - Add field status to booking
    
     - Add enumerize to booking
    
    trello: https://trello.com/c/KV7Ya9gf

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_16_045033) do
+ActiveRecord::Schema.define(version: 2020_02_19_121428) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2020_02_16_045033) do
     t.integer "person"
     t.string "payment_code"
     t.integer "midtrans_id"
+    t.integer "status"
   end
 
   create_table "categories", force: :cascade do |t|
