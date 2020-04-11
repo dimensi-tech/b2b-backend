1ee40fc10840e8b5bf961f9f229e2bb2fb6da0db
Persada Ersad <ersad40@gmail.com>
2020-04-11 13:11:45 +0700

    Add Booking Status
     - Add booking status enumerize on bookings table

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,12 @@
+# frozen_string_literal: true
+
 class Booking < ApplicationRecord
+  extend Enumerize
+
   belongs_to :package
   belongs_to :customer
   belongs_to :voucher
-
   has_many :payment_activities, dependent: :destroy
+
+  enumerize :booking_status, in: { booked: 1, modified: 2, cancelled: 3 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_05_030417) do
+ActiveRecord::Schema.define(version: 2020_04_11_060517) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2020_04_05_030417) do
     t.string "midtrans_id"
     t.string "status"
     t.integer "passport_ids", default: [], array: true
+    t.integer "booking_status"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -64,8 +65,6 @@ ActiveRecord::Schema.define(version: 2020_04_05_030417) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.boolean "is_confirmed"
-    t.datetime "confirmed_at"
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -78,6 +77,8 @@ ActiveRecord::Schema.define(version: 2020_04_05_030417) do
     t.string "confirmation_token"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.boolean "is_confirmed"
+    t.datetime "confirmed_at"
     t.index ["confirmation_token"], name: "index_customers_on_confirmation_token", unique: true
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
