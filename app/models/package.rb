447edbc3192b62685b3c08d6902e3e32896eb0ad
MRomadhan <muhammad.romadhan@c-aio.com>
2020-04-22 03:41:32 +0700

    Saving Package Improved
    Update fitur saving package, add calculation form field
    
    Trello : https://trello.com/c/6YIb47sH

--- app/models/package.rb
+++ app/models/package.rb
@@ -9,4 +9,10 @@ class Package < ApplicationRecord
   accepts_nested_attributes_for :saving_packages, allow_destroy: true
 
   validates :name, :price, :down_payment, :duration_trip, :min_person, presence: true
+
+  def assigned_down_payment
+    if saving_packages.present?
+      update(down_payment: saving_packages.order(amount: :asc).first.amount)
+    end
+  end
 end
