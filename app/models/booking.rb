ebfaf30193ef3802b2fe3678faf396676682d8d6
MRomadhan <muhammad.romadhan@c-aio.com>
2020-10-01 16:52:09 +0700

    Booking Confirmation Mailer
    Add template for mailer booking confirmation
    
    Trello : https://trello.com/c/WO59Dlrf

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -11,4 +11,10 @@ class Booking < ApplicationRecord
   has_many :payment_savings, dependent: :destroy
 
   enumerize :booking_status, in: { booked: 1, modified: 2, cancelled: 3 }
+
+  def calculate_total_booking
+    total_adult_price = adult * package.adult_price
+    total_child_price = child * package.child_price
+    total_adult_price + total_child_price
+  end
 end
