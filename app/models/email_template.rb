98187763de6c2b7cd688f476d2540d447132077d
MRomadhan <muhammad.romadhan@c-aio.com>
2020-10-20 10:43:50 +0700

    Bookoing Status Mailer
    Add template email for booking statuses mailer
    
    Trello : https://trello.com/c/WO59Dlrf

--- app/models/email_template.rb
+++ app/models/email_template.rb
@@ -6,5 +6,8 @@ class EmailTemplate < ApplicationRecord
 
   scope :active, -> { where(status: true) }
 
-  enumerize :template_type, in: %i[payment_finish email_confirmation booking_confirmation], scope: :type
+  enumerize :template_type, in: %i[
+    payment_paid email_confirmation booking_confirmation booking_modified booking_cancelled
+    down_payment savings_progress booking_completed
+  ], scope: :type
 end
