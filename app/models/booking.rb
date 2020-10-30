48d8cebb08615fe30aa57761894c05e22c40f663
MRomadhan <muhammad.romadhan@c-aio.com>
2020-10-30 14:49:28 +0700

    Booking Detail Status
    Upadte booking status enumerize

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -10,7 +10,9 @@ class Booking < ApplicationRecord
   has_many :payment_activities, dependent: :destroy
   has_many :payment_savings, dependent: :destroy
 
-  enumerize :booking_status, in: { booked: 1, modified: 2, cancelled: 3 }
+  enumerize :booking_status, in: { pending: 0, booked: 1, modified: 2, cancelled: 3,
+                                   down_payment: 4, saving_progress: 5, completed: 6,
+                                   paid: 7 }
 
   def calculate_total_booking
     total_adult_price = adult.to_i * package.adult_price.to_i
